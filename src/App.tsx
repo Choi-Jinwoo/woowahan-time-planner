4fa443bc783553f0f52b036d951cf4791f1f2ede
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-08 14:42:43 +0900

    init: React, Typescript Webpack 설정

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/reset.css';
+import './assets/styles/root.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      App HI
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.jsx', '.tsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jp(e*)g)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8000,
+          name: 'images/[hash]-[name].[ext]'
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}
